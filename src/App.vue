7c8be76b44be7d60456fa1047900f1e7714ce510
AntonMakarkin <amakarkin27@gmail.com>
2022-10-27 21:46:01 +0300

    VueJS3 Tutorial - 21 - Modifiers

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,10 @@
       {{ JSON.stringify(formValues, null, 2) }}
     </pre>
   </div>
-  <form @submit="submitForm">
+  <form @submit.prevent="submitForm">
     <div>
       <label for="name">Name</label>
-      <input type="text" id="name" v-model="formValues.name">
+      <input type="text" id="name" v-model.trim.lazy="formValues.name">
     </div>
 
     <div>
@@ -62,8 +62,13 @@
     </div>
 
     <div>
-      <button>Submit</button>
+      <label for="age">Age</label>
+      <input @keyup.enter="submitForm" type="number" id="age" v-model.number="formValues.age">
     </div>
+
+    <!--<div>
+      <button>Submit</button>
+    </div>-->
   </form>
 </template>
 
@@ -79,13 +84,13 @@ export default {
         jobLocation: [],
         remoteWork: 'no',
         skillSet: [],
-        yearsOfExperience: ''
+        yearsOfExperience: '',
+        age: null
       }
     }
   },
   methods: {
-    submitForm(event) {
-      event.preventDefault();
+    submitForm() {
       console.log('Form values', this.formValues)
     }
   }
