691834b2d51a3400a83711702ccc22769df95c46
AntonMakarkin <amakarkin27@gmail.com>
2022-11-04 00:17:45 +0300

    VueJS3 tutorial - 23 - Computed Properties

--- src/App.vue
+++ src/App.vue
@@ -1,75 +1,9 @@
 <template>
-  <div>
-    <pre>
-      {{ JSON.stringify(formValues, null, 2) }}
-    </pre>
-  </div>
-  <form @submit.prevent="submitForm">
-    <div>
-      <label for="name">Name</label>
-      <input type="text" id="name" v-model.trim.lazy="formValues.name">
-    </div>
-
-    <div>
-      <label for="profile">Profile Summary</label>
-      <textarea id="profile" v-model="formValues.profileSummary"/>
-    </div>
-
-    <div>
-      <label for="country">Country</label>
-      <select id="country" v-model="formValues.country">
-        <option value="">Select a country</option>
-        <option value="India">India</option>
-        <option value="Vietnam">Vietnam</option>
-        <option value="Singapore">Singapore</option>
-      </select>
-    </div>
-
-    <div>
-      <label for="job-location">Job Location</label>
-      <select id="job-location" multiple v-model="formValues.jobLocation">
-        <option value="India">India</option>
-        <option value="Vietnam">Vietnam</option>
-        <option value="Singapore">Singapore</option>
-      </select>
-    </div>
-
-    <div>
-      <input type="checkbox" id="remoteWork" v-model="formValues.remoteWork" true-value="yes" false-value="no">
-      <label for="remoteWork">Open to remote work?</label>
-    </div>
-
-    <div>
-      <label>Skill set</label>
-      <input type="checkbox" id="html" value="html" v-model="formValues.skillSet">
-      <label for="html">HTML</label>
-      <input type="checkbox" id="css" value="css" v-model="formValues.skillSet">
-      <label for="css">CSS</label>
-      <input type="checkbox" id="javascript" value="javascript" v-model="formValues.skillSet">
-      <label for="javascript">JavaScript</label>
-    </div>
-
-    <div>
-      <label>Years of Experience</label>
-      <input type="radio" value="0-2" v-model="formValues.yearsOfExperience" />
-      <label>0-2</label>
-      <input type="radio" value="3-5" v-model="formValues.yearsOfExperience" />
-      <label>3-5</label>
-      <input type="radio" value="6-10" v-model="formValues.yearsOfExperience" />
-      <label>5-10</label>
-      <input type="radio" value="10+" v-model="formValues.yearsOfExperience" />
-      <label>10+</label>
-    </div>
-
-    <div>
-      <label for="age">Age</label>
-      <input @keyup.enter="submitForm" type="number" id="age" v-model.number="formValues.age">
-    </div>
-
-    <!--<div>
-      <button>Submit</button>
-    </div>-->
-  </form>
+  <h2>Fullname - {{firstName}} {{lastName}}</h2>
+  <h2>Computed fullname - {{fullName}}</h2>
+  <h2>Total - {{items.reduce((total, curr) => (total = total + curr.price), 0)}}</h2>
+  <h2>Computed total - {{total}}</h2>
+  <button @click="items.push({ id: 4, title: 'Keyboard', price: 50})">Add to card</button>
 </template>
 
 <script>
@@ -77,21 +11,36 @@ export default {
   name: 'App',
   data() {
     return {
-      formValues: {
-        name: '',
-        profileSummary: '',
-        country: '',
-        jobLocation: [],
-        remoteWork: 'no',
-        skillSet: [],
-        yearsOfExperience: '',
-        age: null
-      }
+      firstName: 'Vika',
+      lastName: 'Shipko',
+      items: [
+        {
+          id: 1,
+          title: 'TV',
+          price: 100
+        },
+        {
+          id: 2,
+          title: 'Phone',
+          price: 200
+        },
+        {
+          id: 3,
+          title: 'Laptop',
+          price: 300
+        }
+      ]
     }
   },
   methods: {
-    submitForm() {
-      console.log('Form values', this.formValues)
+
+  },
+  computed: {
+    fullName() {
+      return `${this.firstName} - ${this.lastName}`
+    },
+    total() {
+      return this.items.reduce((total, curr) => (total = total + curr.price), 0)
     }
   }
 }
