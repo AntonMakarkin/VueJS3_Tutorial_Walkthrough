a3a89d201975f87b896db1cb02456703c5fdbcfb
AntonMakarkin <amakarkin27@gmail.com>
2022-11-18 00:01:54 +0300

    Vue JS 3 Tutorial - 29 - Components

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,15 @@
 <template>
-  <h2>Volume Tracker (0-20)</h2>
-  <h3>Current Volume - {{ volume }}</h3>
-  <div>
-    <button @click="volume += 2">Increase</button>
-    <button @click="volume -= 2">Decrease</button>
-  </div>
-  <input type="text" v-model="movie">
-  <input type="text" v-model="movieInfo.title">
-  <input type="text" v-model="movieInfo.actor">
-  <div>
-    <button @click="movieList.push('Wonder Woman')"></button>
-  </div>
+  <Greet/>
 </template>
 
 <script>
+import GreetComponent from './components/Greet.vue'
+
 export default {
   name: 'App',
-  data() {
-    return {
-      volume: 0,
-      movie: 'Batman',
-      movieInfo: {
-        title: '',
-        actor: ''
-      },
-      movieList: ['Batman', 'Superman']
-    }
-  },
-  methods: {},
-  computed: {},
-  watch: {
-    volume(newValue, oldValue) {
-      if (newValue > oldValue && newValue === 16) {
-        alert('Listening to a high volume for a long may damage your hearing')
-      }
-    },
-    movie: {
-      handler(newValue) {
-        console.log(`Calling API with movie name = ${newValue}`) 
-      },
-      immediate: true //run with init page
-    },
-    movieInfo: {
-      handler(newValue) {
-        console.log(`Calling API with movie title = ${newValue.title} and actor = ${newValue.actor}`) 
-      },
-      deep: true // for watching objects and arrays
-    },
-    movieList: {
-      handler(newValue) {
-        console.log(`Updated list ${newValue}`)
-      },
-      deep: true
-    }
-  },
+  components: {
+    Greet: GreetComponent
+  }
 }
 </script>
 
@@ -62,51 +18,8 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  /*text-align: center;*/
+  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-.underline {
-  text-decoration: underline;
-}
-
-.promoted {
-  font-style: italic;
-}
-
-.new {
-  color: olivedrab;
-}
-
-.sold-out {
-  color: red;
-}
-
-label {
-  font-weight: bold;
-  display: flex;
-  margin-bottom: 5px;
-}
-
-input + label {
-  font-weight: bold;
-  display: inline-flex;
-  margin-right: 20px;
-}
-
-input[type='text'],
-textarea,
-select {
-  display: block;
-  width: 400px;
-  padding: 6px 12px;
-  font-size: 14px;
-  line-height: 1.42857143;
-  color: #555;
-  background-color: #fff;
-  background-image: none;
-  border: 1px solid #ccc;
-  border-radius: 4px;
 }
 </style>
---
+++ src/components/Greet.vue
@@ -0,0 +1,15 @@
+<template>
+    <h2>
+        Hello Anton
+    </h2>
+</template>
+
+<script>
+    export default {
+        name: 'GreetComponent'
+    }
+</script>
+
+<style scoped>
+
+</style>
