1b02b6e7ea89a4dcbd80e1e0624b239d5869e24c
AntonMakarkin <amakarkin27@gmail.com>
2022-11-19 00:23:35 +0300

    Vue JS 3 Tutorial - 33 - Provide and Inject

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,35 @@
 <template>
-  <Greet name="Anton" surname="Makarkin"/>
+<!--<Greet name="Anton" surname="Makarkin"/>
   <Greet name="Vika" surname="Shipko"/>
   <Greet name="Vova" surname="Maksin"/>
   <Greet :name="name" :surname="channel"/>
 
   <Article id="my-article" title="Article Title" :likes="50" :isPublished="true"/>
+  -->
+  <h3>AppComponent username {{ name }}</h3>
+  <ComponentC/>
 </template>
 
 <script>
-import GreetComponent from './components/Greet.vue';
-import Article from './components/Article.vue'
+//import GreetComponent from './components/Greet.vue';
+//import Article from './components/Article.vue'
+import ComponentC from './components/ComponentC'
 
 export default {
   name: 'App',
   components: {
-    Greet: GreetComponent,
-    Article
+    ComponentC
+    //Greet: GreetComponent,
+    //Article
   },
   data() {
     return {
-      name: 'Vishwas',
-      channel: 'Codevolution'
+      name: 'Anton'
+    }
+  },
+  provide() {
+    return {
+      username: this.name
     }
   }
 }
