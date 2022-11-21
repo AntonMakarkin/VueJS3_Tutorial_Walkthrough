7c6c5af398bcd751527725cf25b1c69e1d7a7fb1
AntonMakarkin <amakarkin27@gmail.com>
2022-11-21 13:44:54 +0300

    Vue JS 3 Tutorial - 38 - Named slots

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,7 +1,15 @@
 <template>
     <div>
         <div class="card">
-            <slot>Default content</slot>
+            <div id="card-header">
+                <slot name="header"></slot>
+            </div>
+            <div id="card-content">
+                <slot></slot>
+            </div>
+            <div id="card-footer">
+                <slot name="footer"></slot>
+            </div>
         </div>
     </div>
 </template>
